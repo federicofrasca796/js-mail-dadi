7f4da7059e7e5b336b1a94717e8923735ebe009a
Federico Frascà <70528215+federicofrasca796@users.noreply.github.com>
2021-10-14 16:53:57 +0200

    fix MAIL excercise | start Dice Game

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,35 +2,35 @@
 MAIL
 Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
 */
-
 // Strumenti: prompt | array (lista di chi può accedere) | IF condition | createElement (h1) -> innerHTML/insertAdjacentHTML
-// imposto una lista (array) di email di utenti che possono accedere.
-const allowedUsers = ['federico.frasca796@gmail.com', 'marianoilvillano@tiscali.it', 'tizianotutor@hotmail.com']
-// console.log(allowedUsers);
 
-// prendo l'email
-const email = prompt("What's your email?")
+//Imposto una lista (array) di email di utenti che possono accedere.
+const allowedUsers = ['federico.frasca796@gmail.com', 'marianoilvillano@tiscali.it', 'tizianotutor@hotmail.com'];
 
-//controllo se l'email inserita dall'utente rientra tra quelle concesse
+//Prendo l'email
+const email = prompt("What's your email?");
+
+//Imposto una variabile sentinella fuori dal ciclo for. Questo mi serve per poterla richiamare fuori, al termine del ciclo, quando dovrò stampare a schermo il risultato.
+let check = false;
+
+//Controllo se l'email inserita dall'utente rientra tra quelle concesse
 for (let i = 0; i < allowedUsers.length; i++) {
-    // console.log(allowedUsers[i])
-
-    //controllo se l'elemento dell'array attualmente selezionato combaci con l'email inserita dall'utente
+    // controllo se l'elemento dell'array attualmente selezionato combaci con l'email inserita dall'utente
     if (email === allowedUsers[i]){
-        console.log('Sei dentro')
-        //mostra a schermo l'esito del check.
-        const Pass = `<i>${email}</i> è nella mia lista. Sei dentro!`;
-        // body.insertAdjacentHTML('afterbegin', Pass); //NOT WORKING
-        document.getElementById('mailResult').innerHTML = Pass
-        
-    } else {
-        console.log('Sei fuori')
-        //mostra a schermo l'esito del check.
-        const notPass = `Non ho mai sentito parlare di <i>${email}</i>, mi spiace sei fuori.`;
-        document.getElementById('mailResult').innerHTML = notPass
+        check = true; 
     }
-    
 }
+
+//Mostra a schermo l'esito del check.
+// console.log(check);
+if (check == true){
+    const Pass = `Welcome back <i>${email}</i>, good to see you again.`;
+    document.getElementById('mailResult').insertAdjacentHTML('afterbegin', Pass);
+} else{
+    const notPass = `I've never heard of <i>${email}</i>. Sorry, you're out.`;
+    document.getElementById('mailResult').insertAdjacentHTML('afterbegin', notPass);
+}
+
 
 
 /*
@@ -38,4 +38,10 @@ GIOCO DEI DADI
 Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 Stabilire il vincitore, in base a chi fa il punteggio più alto.
 */
+
+//genero un numero random e lo inserisco dentro una variabile. Ripeto l'azione per due volte, una per il PC e una per il player.
+// const playerValue = 
+//controllo i due valori, se il valore uscito al player è maggiore di quello del PC, vince il player.
+//altrimenti, vince il PC.
+
 
